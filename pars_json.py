bbdb24f88ef7ee8ba77b174a6597ce3f844509ae
Sergey <sebastyan-hoy@yandex.ru>
2023-06-27 02:37:37 +0300

    Получаем значение текста второго объекта массива

---
+++ pars_json.py
@@ -0,0 +1,13 @@
+import json
+json_text='{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+# Наша задача с помощью библиотеки “json”, которую мы показывали на занятии, распарсить нашу переменную json_text и
+# вывести текст второго сообщения с помощью функции print.
+
+obj=json.loads(json_text)
+key0="messages"
+position=1 # если укажем 0, то получим первое сообщение
+key1="message" # если укажем timestamp, то получим дату сообщения
+
+print(obj[key0][position][key1])
+
+
