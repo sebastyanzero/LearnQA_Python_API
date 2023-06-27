9526a640c793d822200f33be9fed579e07ca450a
Sergey <sebastyan-hoy@yandex.ru>
2023-06-27 15:53:37 +0300

    Считаем редиректы и выводим итоговый урл

--- redirect.py
+++ redirect.py
@@ -3,7 +3,7 @@
 # от изначальной точки назначения до итоговой. И какой URL итоговый.
 import requests
 head= {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 YaBrowser/23.5.2.625 Yowser/2.5 Safari/537.36"}
-resp=requests.get("https://playground.learnqa.ru/api/long_redirect", headers=head)
+resp=requests.get("https://playground.learnqa.ru/api/long_redirect", headers=head, allow_redirects=True)
 count_redir=len(resp.history) #считаем количество
 #Перебираем все исторические записи
 for i in range(count_redir):
